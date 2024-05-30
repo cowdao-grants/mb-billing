ecd58d47301cb433f434b4e56516bcb11da1331c
cowanator <167968501+cowanator@users.noreply.github.com>
2024-05-30 13:00:13 +0200

    remove .only test

--- tests/e2e/e2e.test.ts
+++ tests/e2e/e2e.test.ts
@@ -37,7 +37,7 @@ describe("e2e - Sepolia", () => {
     expect(logs!.length).toEqual(1);
   });
 
-  it.only("Runs the drafting flow with mainnet data on Sepolia billing contract", async () => {
+  it("Runs the drafting flow with mainnet data on Sepolia billing contract", async () => {
     const paymentStatus = await dataFetcher.getPaymentStatus();
     const sudoBillingContract = BillingContract.fromEnv(true);
     const draftingHashes =
